4230538b1d85aa5735cdabf61318eba30bcf570d
Maxim Osipovs <maximosipovs@MacBook-Air-von-Maxim.local>
2023-03-05 15:29:02 +0100

    initial commit

---
+++ .config/nvim/lua/user/nvimtree.lua
@@ -0,0 +1,11 @@
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+  return
+end
+
+require("nvim-tree").setup()
